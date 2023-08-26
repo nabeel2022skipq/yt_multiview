178883611e1f5b227ce421e7964f7f0b55a7c525
nabeelahmed-0 <nabeelahmedpk1@outlook.com>
2023-08-26 22:15:30 +0500

    Initial commit

---
+++ server.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb+srv://ytmultiview:DFnHdTnpmKX5BxvA@cluster0.0hprzrn.mongodb.net/YTMultiView?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+app.use(express.json());
+
+const multiViewSchema = new mongoose.Schema({
+    url: String,
+    videos: [String],
+});
+
+const MultiView = mongoose.model('MultiView', multiViewSchema);
+
+app.get('/', async (req, res) => {
+    res.send("Hello World")
+})
+
+app.post('/api/multiViews', async (req, res) => {
+    const { url, videos } = req.body;
+
+    try {
+        const multiView = await MultiView.create({ url, videos });
+        res.json(multiView);
+    } catch (error) {
+        res.status(500).json({ error: 'Could not create multi-view' });
+    }
+});
+
+app.get('/api/multiViews/:uuid', async (req, res) => {
+    const uuid = req.params.uuid;
+
+    try {
+        const multiView = await MultiView.findOne({ url: uuid });
+        if (multiView) {
+            res.json(multiView);
+        } else {
+            res.status(404).json({ error: 'Multi-view not found' });
+        }
+    } catch (error) {
+        res.status(500).json({ error: 'Could not fetch multi-view' });
+    }
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
